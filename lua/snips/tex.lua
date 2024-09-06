6f22ac7ad0f2d177511cd23232d6fc0ebc030eb8
Sean Li <seanli646@gmail.com>
2024-09-06 10:24:28 -0400

    modified tex luasnip

--- lua/snips/tex.lua
+++ lua/snips/tex.lua
@@ -7,137 +7,147 @@ local fmta = require("luasnip.extras.fmt").fmta
 local rep = require("luasnip.extras").rep
 
 local ret = {
-  s(
-    { trig = "fr", dscr = "Fraction" },
-    fmta("\\frac{<>}{<>}", {
-      i(1),
-      i(2),
-    })
-  ),
-  s(
-    { trig = "ov", dscr = "Overset" },
-    fmta("\\overset{<>}{<>}", {
-      i(1),
-      i(2),
-    })
-  ),
-  s(
-    { trig = "bb" },
-    fmta(
-      [[
+	s({ trig = "bs", dscr = "Backslash" }, t("\\backslash")),
+	s(
+		{ trig = "fr", dscr = "Fraction" },
+		fmta("\\frac{<>}{<>}", {
+			i(1),
+			i(2),
+		})
+	),
+	s(
+		{ trig = "ov", dscr = "Overset" },
+		fmta("\\overset{<>}{<>}", {
+			i(1),
+			i(2),
+		})
+	),
+	s(
+		{ trig = "bb" },
+		fmta(
+			[[
       \begin{<>}
         <>
       \end{<>}
     ]],
-      {
-        i(1),
-        i(2),
-        rep(1),
-      }
-    )
-  ),
-  s(
-    { trig = "ci" },
-    fmta("\\cite{<>}", {
-      i(1),
-    })
-  ),
-  s(
-    { trig = "ma" },
-    fmta(
-      [[
+			{
+				i(1),
+				i(2),
+				rep(1),
+			}
+		)
+	),
+	s(
+		{ trig = "ci" },
+		fmta("\\cite{<>}", {
+			i(1),
+		})
+	),
+	s(
+		{ trig = "ma" },
+		fmta(
+			[[
       \left(\begin{array}{<>}
         <>
       \end{array}\right)
     ]],
-      {
-        i(1),
-        i(2),
-      }
-    )
-  ),
-  s(
-    { trig = "enn" },
-    fmta(
-      [[
+			{
+				i(1),
+				i(2),
+			}
+		)
+	),
+	s(
+		{ trig = "enn" },
+		fmta(
+			[[
     \begin{enumerate}[label=(\arabic*)]
       \item <>
     \end{enumerate}]],
-      { i(1) }
-    )
-  ),
-  s(
-    { trig = "ena" },
-    fmta(
-      [[
+			{ i(1) }
+		)
+	),
+	s(
+		{ trig = "ena" },
+		fmta(
+			[[
     \begin{enumerate}[label=(\alph*)]
       \item <>
     \end{enumerate}]],
-      { i(1) }
-    )
-  ),
-  s(
-    { trig = "it" },
-    fmta(
-      [[
+			{ i(1) }
+		)
+	),
+	s(
+		{ trig = "it" },
+		fmta(
+			[[
     \begin{itemize}
       \item <>
     \end{itemize}]],
-      { i(1) }
-    )
-  ),
-  s(
-    { trig = "lr" },
-    fmta([[\left<> <> \right<>]], {
-      i(1),
-      i(2),
-      f(function(args)
-        local close = { ["("] = ")", ["\\{"] = "\\}", ["|"] = "|", ["["] = "]", ["\\|"] = "\\|" }
-        local ret = close[args[1][1]]
-        if ret == nil then
-          return ""
-        end
-        return ret
-      end, { 1 }),
-    })
-  ),
+			{ i(1) }
+		)
+	),
+	s(
+		{ trig = "lr" },
+		fmta([[\left<> <> \right<>]], {
+			i(1),
+			i(2),
+			f(function(args)
+				local close = { ["("] = ")", ["\\{"] = "\\}", ["|"] = "|", ["["] = "]", ["\\|"] = "\\|" }
+				local ret = close[args[1][1]]
+				if ret == nil then
+					return ""
+				end
+				return ret
+			end, { 1 }),
+		})
+	),
 }
 
-for k, v in pairs({ cu = "bigcup", ca = "bigcap", su = "sum", ["in"] = "int" }) do
-  table.insert(ret, s({ trig = k }, fmta([[\<>_{<>}]], { t(v), i(1) })))
+local big = {
+	cu = "bigcup",
+	ca = "bigcap",
+	su = "sum",
+	li = "lim",
+	["in"] = "int",
+}
+
+for k, v in pairs(big) do
+	table.insert(ret, s({ trig = k }, fmta([[\<>_{<>}]], { t(v), i(1) })))
 end
 
 local envs = {
-  mm = "align*",
-  pf = "proof",
-  ml = "multline*",
-  ca = "cases",
-  rm = "remark",
-  th = "theorem",
-  lm = "lemma",
-  pr = "proposition",
-  co = "corollary",
+	mm = "align*",
+	pf = "proof",
+	ml = "multline*",
+	ca = "cases",
+	re = "remark",
+	th = "theorem",
+	le = "lemma",
+	pr = "proposition",
+	co = "corollary",
+	de = "definition",
 }
 
 for k, v in pairs(envs) do
-  table.insert(
-    ret,
-    s(
-      { trig = k },
-      fmta(
-        [[
+	table.insert(
+		ret,
+		s(
+			{ trig = k },
+			fmta(
+				[[
       \begin{<>}
         <>
       \end{<>}
     ]],
-        {
-          t(v),
-          i(1),
-          t(v),
-        }
-      )
-    )
-  )
+				{
+					t(v),
+					i(1),
+					t(v),
+				}
+			)
+		)
+	)
 end
 
 return ret
